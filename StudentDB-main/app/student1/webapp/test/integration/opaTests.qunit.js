e3a978bb2ef519d3cd9f3396d0d87a73aac2b884
Bhanu Prakash Dudikatla <bhanududikatla@gmail.com>
2024-02-07 08:06:01 +0530

    example of 1st commit

---
+++ StudentDB-main/app/student1/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'student1/test/integration/FirstJourney',
+		'student1/test/integration/pages/StudentList',
+		'student1/test/integration/pages/StudentObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, StudentList, StudentObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('student1') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheStudentList: StudentList,
+					onTheStudentObjectPage: StudentObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
